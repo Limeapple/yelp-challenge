36f63c40afb5341ce1ba9a2296e8064db7953d81
Kevin Liu <kevinliu54321@gmail.com>
2020-03-08 23:00:24 -0500

    Updates

---
+++ precache-manifest.df9a8635de958f71d699b585a6728122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f7285b1e77b7ca878739d446c63044",
+    "url": "/yelp-challenge/index.html"
+  },
+  {
+    "revision": "5512d04c499fe8969946",
+    "url": "/yelp-challenge/static/css/main.783ac5c6.chunk.css"
+  },
+  {
+    "revision": "c408879984c6dd89f2c3",
+    "url": "/yelp-challenge/static/js/2.4264e662.chunk.js"
+  },
+  {
+    "revision": "acf6b8c8151ff08b8ff98fca15e1d118",
+    "url": "/yelp-challenge/static/js/2.4264e662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5512d04c499fe8969946",
+    "url": "/yelp-challenge/static/js/main.a5cb6ccd.chunk.js"
+  },
+  {
+    "revision": "d3e1bbd2ed36da6736c7",
+    "url": "/yelp-challenge/static/js/runtime-main.1bbca75a.js"
+  },
+  {
+    "revision": "1860fd967c958a12ade54722dd2205ec",
+    "url": "/yelp-challenge/static/media/peopleEating.1860fd96.jpg"
+  }
+]);
